2a5d82c43ce36d11ede81ffdcec40f2984ca7d94
cambridgewatch <universe7903@gmail.com>
2026-07-23 22:05:28 +0900

    Add Cheonhoji runner course background

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dku-med26-app-v72";
+const CACHE_NAME = "dku-med26-app-v73";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -20,6 +20,7 @@ const APP_SHELL = [
   "./assets/img/icon-192.png",
   "./assets/img/icon-512.png",
   "./assets/img/icon-maskable-512.png",
+  "./assets/img/cheonhoji-run-background-v1.png",
   "./assets/js/main.js",
   "./assets/js/session-hint.js",
   "./assets/js/password-visibility.js",
